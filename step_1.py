1d4c7a2f0caf4e1b54fb3cd9469f22a589973392
Алексей <aiex.0115970@gmail.com>
2021-06-07 11:19:10 +0500

    1

---
+++ step_1.py
@@ -0,0 +1,14 @@
+# имя
+# дата рождения
+# адрес
+
+class Student:
+    name = 'Алексей'
+
+    dob = '19.03.2002'
+
+    address = 'ул. Ленина, 113'
+
+    def display_info(self):
+        print('Привет', self.name)
+        print('Дата', self.name)
