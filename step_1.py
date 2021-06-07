5fd49587d82446fba9539040351c8487f16a9f16
Алексей <aiex.0115970@gmail.com>
2021-06-07 11:20:41 +0500

    1

--- step_1.py
+++ step_1.py
@@ -11,4 +11,9 @@ class Student:
 
     def display_info(self):
         print('Привет', self.name)
-        print('Дата', self.name)
+        print('Дата рождения', self.dob)
+        print('Адресс', self.address)
+
+
+student_1 = Student()
+student_1.display_info()
