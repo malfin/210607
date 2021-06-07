ac7ac073f5a4e0b153a46583ac203aa8552f9770
Алексей <aiex.0115970@gmail.com>
2021-06-07 11:21:04 +0500

    2

--- step_1.py
+++ step_1.py
@@ -10,8 +10,8 @@ class Student:
     address = 'ул. Ленина, 113'
 
     def display_info(self):
-        print('Привет', self.name)
-        print('Дата рождения', self.dob)
+        print('Привет, ', self.name)
+        print('Дата рождения:', self.dob)
         print('Адресс', self.address)
 
 
